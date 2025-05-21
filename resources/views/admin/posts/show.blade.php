794022bb63021c3ef9e976a3b5a9083ddb59c3c3
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2025-05-21 15:35:11 +0700

    feat: show post

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,16 +1,16 @@
 @extends('adminlte::layouts.app')
 @section('htmlheader_title')
-    {{ __('categories.category') }}
+    {{ __('posts.category') }}
 @endsection
 @section('contentheader_title')
-    {{ __('categories.category') }}
+    {{ __('posts.category') }}
 @endsection
 @section('contentheader_description')
 @endsection
 @section('breadcrumb')
     <ol class="breadcrumb">
         <li><a href="{{ url('admin') }}"><i class="fa fa-home"></i> {{ __('message.dashboard') }}</a></li>
-        <li><a href="{{ url('/admin/videos') }}">{{ __('categories.category') }}</a></li>
+        <li><a href="{{ url('/admin/posts') }}">{{ __('posts.category') }}</a></li>
         <li class="active">{{ __('message.detail') }}</li>
     </ol>
 @endsection
@@ -19,17 +19,17 @@
         <div class="box-header">
             <h3 class="box-title">{{ __('message.detail') }}</h3>
             <div class="box-tools">
-                <a href="{{ url('/admin/videos') }}" class="btn btn-warning btn-sm"><i class="fa fa-arrow-left"
+                <a href="{{ url('/admin/posts') }}" class="btn btn-warning btn-sm"><i class="fa fa-arrow-left"
                         aria-hidden="true"></i> <span class="hidden-xs">{{ trans('message.lists') }}</span></a>
-                @can('VideoController@update')
-                    <a href="{{ url('/admin/videos/' . $video->id . '/edit') }}" class="btn btn-primary btn-sm"><i
+                @can('PostController@update')
+                    <a href="{{ url('/admin/posts/' . $post->id . '/edit') }}" class="btn btn-primary btn-sm"><i
                             class="fa fa-pencil-square-o" aria-hidden="true"></i> <span
                             class="hidden-xs">{{ __('message.edit') }}</span></a>
                 @endcan
-                @can('VideoController@destroy')
+                @can('PostController@destroy')
                     {!! Form::open([
                         'method' => 'DELETE',
-                        'url' => ['admin/videos', $video->id],
+                        'url' => ['admin/posts', $post->id],
                         'style' => 'display:inline',
                     ]) !!}
                     {!! Form::button(
@@ -49,23 +49,35 @@
             <table class="table table-striped">
                 <tbody>
                     <tr>
-                        <th> {{ trans('theme::categories.title') }} </th>
-                        <td> {{ $video->title }} </td>
+                        <th> {{ trans('theme::posts.title') }} </th>
+                        <td> {{ $post->title }} </td>
                     </tr>
 
 
                     <tr>
-                        <th> {{ trans('theme::categories.url') }} </th>
-                        <td> <a href="">{{ $video->pathfile }}</a> </td>
+                        <th> {{ trans('theme::posts.url') }} </th>
+                        <td> <a href="/{{$post->url}}">{{ $post->url }}</a> </td>
+
+                    </tr>
+
+                    <tr>
+                        <th> {{ trans('theme::posts.summary') }} </th>
+                        <td> {{ $post->summary }} </td>
+
+                    </tr>
+
+                    <tr>
+                        <th> {{ trans('theme::posts.content') }} </th>
+                        <td> {{ $post->content }} </td>
 
                     </tr>
                     <tr>
                         <th> {{ trans('Sắp xếp') }} </th>
-                        <td> {{ $video->arrange }} </td>
+                        <td> {{ $post->arrange }} </td>
                     </tr>
                       <tr>
-                        <th> {{ trans('theme::categories.active') }} </th>
-                        <td> {{ $video->approved == 1 ? 'Duyệt' : "Chưa duyệt" }} </td>
+                        <th> {{ trans('theme::posts.active') }} </th>
+                        <td> {{ $post->approved == 1 ? 'Duyệt' : "Chưa duyệt" }} </td>
                     </tr>
 
                 </tbody>
